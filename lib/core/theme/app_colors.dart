e5d989dcb1980c8c578a24e87ca8b47d2e4d3028
Kaycatghost <kamilymilena16@gmail.com>
2026-09-16 18:15:22 -0300

    Refactor AppColors class to be abstract

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
-class AppColors {
-  // Cores principais
-  static const Color primary = Color(0xFF0757A6);
-  static const Color secondary = Color(0xFF4CAF3D);
+abstract class AppColors {
+  // Principais
+  static const blue = Color(0xFF0755B8);
+  static const green = Color(0xFF69BE45);
 
   // Fundos
-  static const Color background = Color(0xFFF7F9FC);
-  static const Color surface = Colors.white;
+  static const white = Colors.white;
+  static const background = Color(0xFFF7F9FC);
+  static const surface = Colors.white;
 
   // Textos
-  static const Color textPrimary = Color(0xFF17365D);
-  static const Color textSecondary = Color(0xFF667085);
+  static const textPrimary = Color(0xFF17365D);
+  static const textSecondary = Color(0xFF667085);
 
-  // Elementos auxiliares
-  static const Color border = Color(0xFFD9E2EC);
-  static const Color lightBlue = Color(0xFFEAF3FB);
-  static const Color lightGreen = Color(0xFFEAF6E8);
+  // Auxiliares
+  static const border = Color(0xFFD9E2EC);
+  static const lightBlue = Color(0xFFEAF3FB);
+  static const lightGreen = Color(0xFFEAF6E8);
 }
